fe1cec51d3158f639da5627db94f0a004fd0d217
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-03-29 15:49:09 +0000

    Update System 3.1

--- browser.bat
+++ browser.bat
@@ -4,12 +4,11 @@ title Spiral 0.10
 echo 1. Check for Updates
 echo 2. Continue
 set /p act= OPTION:
-if %OPT% == 1 goto update
+if %OPT% == 1 call updsys.bat
+if %OPT% == 2 goto cnt
+:cnt
 set /p URL= URL:
 curl https://%URL%
 pause
 cls
 goto GTS
-:update
-echo Downloading Update...
-call updsys.bat
