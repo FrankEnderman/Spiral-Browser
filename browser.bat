f96add92fdadef8174d17a85e8cb6a9a2cf9b1d5
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-03-30 15:46:05 +0000

    add 0.30 NeXT previews

--- browser.bat
+++ browser.bat
@@ -1,10 +1,13 @@
 @echo off
 :GTS
-title Spiral 0.20
+title Spiral 0.30 NeXT (Preview 2.1)
+color 02
 curl https://raw.githubusercontent.com/FrankEnderman/Spiral-Browser/main/DEPNOTICE
+echo --------------------------------------------------------------
 echo For the latest updates, redownload it using GitHub. 
 
 set /p URL= URL:
+echo.
 curl https://%URL%
 pause
 cls
