30ec38cc43cd5608a404fbcc2ae1b899b4783f86
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-03-31 15:33:40 +0000

    add download component

--- browser.bat
+++ browser.bat
@@ -6,7 +6,13 @@ color 02
 curl https://raw.githubusercontent.com/FrankEnderman/Spiral-Browser/main/DEPNOTICE
 echo --------------------------------------------------------------
 echo For the latest updates, run Updater (updsys.bat)
-
+echo.
+echo 1. Print URL
+echo 2. Download
+set /p OPT1= OPTION: 
+if %OPT1% == 1 goto printurl
+if %OPT1% == 2 goto downurl
+:printurl
 set /p URL= URL:
 echo.
 curl https://www.%URL%
@@ -15,3 +21,9 @@ echo.
 pause
 cls
 goto MAIN
+:downurl
+set /p URL1= URL:
+set /p NM= FileName:
+curl %URL% >> %NM%
+pause 
+goto main
